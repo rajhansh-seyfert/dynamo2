b7d0f9eca3a1ef7685cf8d8ededa1cf0597e172e
vinay kumar- <vchinnakotla@quinovas.com>
2018-06-01 10:45:09 +0530

    gso added

--- dyanmobasic.tf
+++ dyanmobasic.tf
@@ -20,6 +20,17 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     attribute_name = "TimeToExist"
     enabled = false
   }
+  
+   global_secondary_index {
+    name               = "joining_dateIndex"
+    hash_key           = "joining_date"
+    range_key          = "joining_date"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["EmployeeId"]
+  }
+
 
   tags {
     Name        = "Seyfert"
